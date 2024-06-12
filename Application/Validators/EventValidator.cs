905db7bf498d3c5d93c77c8268b6090dee86fad6
Usov <usov.lesha2017@gmail.com>
2024-06-12 14:49:34 +0300

    create validator

---
+++ Application/Validators/EventValidator.cs
@@ -0,0 +1,17 @@
+﻿using EventManagement.Domain.Entities;
+using FluentValidation;
+
+namespace EventManagement.Application.Validators;
+
+public class EventValidator:AbstractValidator<Event>
+{
+    public EventValidator()
+    {
+        RuleFor(e=>e.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(e=>e.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(e=>e.Date).NotEmpty().WithMessage("Date is required");
+        RuleFor(e=>e.Location).NotEmpty().WithMessage("Location is required");
+        RuleFor(e=>e.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(e=>e.MaxParticipants).NotEmpty().WithMessage("MaxParticipants is required");
+    }
+}
---
+++ Application/Validators/ParticipantValidator.cs
@@ -0,0 +1,16 @@
+﻿using EventManagement.Domain.Entities;
+using FluentValidation;
+
+namespace EventManagement.Application.Validators;
+
+public class ParticipantValidator:AbstractValidator<Participant>
+{
+    public ParticipantValidator()
+    {
+        RuleFor(p=>p.FirstName).NotEmpty().WithMessage("First Name is required");
+        RuleFor(p=>p.LastName).NotEmpty().WithMessage("Last Name is required");
+        RuleFor(p=>p.Email).NotEmpty().WithMessage("Email is required");
+        RuleFor(p=>p.Email).EmailAddress().WithMessage("Email is not valid");
+        RuleFor(p=>p.BirthDate).NotEmpty().WithMessage("Birth Date is required");   
+    }
+}
