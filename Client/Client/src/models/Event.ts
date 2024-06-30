ea15c57acf719c50674066c5433730e69e5dc475
Usov <usov.lesha2017@gmail.com>
2024-06-30 12:35:44 +0300

    add Events DTO And add events functions fetch one event and create event

--- Client/Client/src/models/Event.ts
+++ Client/Client/src/models/Event.ts
@@ -8,3 +8,13 @@
     maxParticipants:number;
     imageSrc:string;
 }
+
+export interface IEventCreate {
+    name: string;
+    description: string;
+    dateTime: Date;
+    location: string;
+    category: string;
+    maxParticipants: number;
+    imageSrc: string;
+}
--- Client/Client/src/services/EventsService.ts
+++ Client/Client/src/services/EventsService.ts
@@ -1,9 +1,15 @@
 ﻿import {AxiosResponse} from "axios";
 import $api from "../http";
-import {IEvent} from "../models/Event";
+import {IEvent, IEventCreate} from "../models/Event";
 
 export default  class EventsService{
     static fetchEvents(page:number):Promise<AxiosResponse>{
         return $api.get<AxiosResponse<IEvent>>(`/Events/events/${page}`);
+    }
+    static fetchEvent(id: number): Promise<AxiosResponse>{
+        return $api.get<AxiosResponse<IEvent>>(`/Events/event/${id}`);
+    }
+    static createEvent(event: IEventCreate): Promise<AxiosResponse> {
+        return $api.post<AxiosResponse<IEventCreate>>('/Events/create', event);
     }
 } 
