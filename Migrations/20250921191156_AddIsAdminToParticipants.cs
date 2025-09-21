a0c6bfb718a7c211caafb60a2eba420ff4455638
alrx31 <usov.lesha2017@gmail.com>
2025-09-21 22:12:15 +0300

    add migration

---
+++ Migrations/20250921191156_AddIsAdminToParticipants.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsAdminToParticipants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
